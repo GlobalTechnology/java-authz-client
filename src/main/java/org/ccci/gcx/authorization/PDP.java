ad5daac17abae3253d8a7fb3920e88b8a67853d8
Daniel Frett <daniel.frett@ccci.org>
2008-04-23 21:05:16 +0000

    stub for PDP source

---
+++ src/main/java/org/ccci/gcx/authorization/PDP.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.ccci.gcx.authorization;
+
+/**
+ * @author frett
+ *
+ */
+public class PDP {
+
+	/**
+	 * 
+	 */
+	public PDP() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

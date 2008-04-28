17e7b99076adf7bb4735e774923e9663445edcf6
Daniel Frett <daniel.frett@ccci.org>
2008-04-28 20:36:42 +0000

    added some base PDP code to check authorization request against GCX server

--- src/main/java/org/ccci/gcx/authorization/PDP.java
+++ src/main/java/org/ccci/gcx/authorization/PDP.java
@@ -3,17 +3,48 @@
  */
 package org.ccci.gcx.authorization;
 
+import org.apache.commons.httpclient.*;
+import org.apache.commons.httpclient.methods.*;
+
 /**
  * @author frett
  *
  */
 public class PDP {
 
+	private HttpClient client;
+
 	/**
 	 * 
 	 */
 	public PDP() {
-		// TODO Auto-generated constructor stub
+		this.client = new HttpClient();
 	}
 
+	/**
+	 * @param entity authorization entity to check authorization for
+	 * @param target authorization target to check authorization for
+	 * @return boolean indicating whether entity has access to target, defaults to false if an error is encountered
+	 */
+	public boolean check(String entity, String target) {
+
+		//generate POST request
+		String requestXML = "<?xml version='1.0' encoding='UTF-8'?><auth_question><entity name='" + entity + "'><target name='" + target + "'/></entity></auth_question>";
+		PostMethod request = new PostMethod("http://dev.mygcx.org/authz");
+		request.addParameter("auth_question", requestXML);
+
+		//try executing the request
+		try {
+			this.client.executeMethod(request);
+		}
+		//we had an error while trying to execute the request
+		catch (Exception e) {
+			
+		}
+		finally {
+			
+		}
+		// default to false
+		return false;
+	}
 }

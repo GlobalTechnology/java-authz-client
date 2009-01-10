7eda70287c69a5782e5772e7e9487d02a0457a48
Daniel Frett <daniel.frett@ccci.org>
2009-01-10 01:48:54 +0000

    added a new check method that will accept a list of targets to check updated httpclient and httpcore to the lastest version of the libraries

--- src/main/java/org/ccci/gcx/authorization/PDP.java
+++ src/main/java/org/ccci/gcx/authorization/PDP.java
@@ -4,6 +4,8 @@
 package org.ccci.gcx.authorization;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -59,11 +61,43 @@ public class PDP {
 	 * @return boolean indicating whether entity has access to target, defaults to false if an error is encountered
 	 */
 	public boolean check(String entity, String target) {
-		//generate the authorization check params
-		String requestXML = "<?xml version='1.0' encoding='UTF-8'?><auth_question><entity name='" + entity + "'><target name='" + target + "'/></entity></auth_question>";
-		List <NameValuePair> params = new ArrayList <NameValuePair>();
-        params.add(new BasicNameValuePair("auth_question", requestXML));
+		//create list to hold the 1 target being checked
+		ArrayList<String> targets = new ArrayList<String>(1);
+		targets.add(target);
+		
+		//process check and return results
+		return check(entity, targets).get(0).booleanValue();
+	}
 
+	
+	/**
+	 * @param entity the entity that authorization is being checked for
+	 * @param targets a list of targets to check authorization for 
+	 * @return List<Boolean> a list of Boolean objects in the same order as the provided list of targets, method uses ArrayList to create List being returned
+	 */
+	public List<Boolean> check(String entity, List<String> targets) {
+		//skip processing if there are no targets to test
+		if(targets.isEmpty()) {
+			return new ArrayList<Boolean>();
+		}
+		
+		//generate the request xml
+		String requestXML = "<?xml version='1.0' encoding='UTF-8'?><auth_question><entity name='" + entity + "'>";
+		//iterate over targets list appending each String in the list as a target
+		Iterator<String> i = targets.iterator();
+		while(i.hasNext()) {
+			requestXML += "<target name='" + i.next() + "'/>";
+		}
+		//close request xml
+		requestXML += "</entity></auth_question>";
+
+		//create a key-value table to store responses
+		HashMap<String,Boolean> responses = new HashMap<String,Boolean>((int)(targets.size()/0.75) + 1, (float)0.75);
+		
+		//generate the parameters for the request 
+		List <NameValuePair> params = new ArrayList <NameValuePair>();
+		params.add(new BasicNameValuePair("auth_question", requestXML));
+		
 		//try executing the request and parsing the response
 		try {
 			//generate POST request
@@ -81,30 +115,45 @@ public class PDP {
 
 			//iterate over all found entity nodes
 			for(int x = 0; x < entities.getLength(); x++) {
-
 				//find the correct entity node
 				if(this.xpathEngine.evaluate("@name", entities.item(x)).equalsIgnoreCase(entity)) {
 					//locate all target nodes
-					NodeList targets = (NodeList) this.xpathEngine.evaluate("target", entities.item(x), XPathConstants.NODESET);
+					NodeList XMLtargets = (NodeList) this.xpathEngine.evaluate("target", entities.item(x), XPathConstants.NODESET);
+
 					//iterate over all target nodes
-					for(int y = 0; y < targets.getLength(); y++) {
-						//is this the requested target?
-						if(this.xpathEngine.evaluate("@name", targets.item(y)).equalsIgnoreCase(target)) {
-							//test if the entity is authorized for this target
-							return (Boolean) this.xpathEngine.evaluate(". = 'yes'", targets.item(y), XPathConstants.BOOLEAN); 
-						}
+					for(int y = 0; y < XMLtargets.getLength(); y++) {
+						//extract the name and whether the entity has access to the target
+						String name = this.xpathEngine.evaluate("@name", XMLtargets.item(y)).toLowerCase();
+						Boolean isAuthorized = (Boolean) this.xpathEngine.evaluate(". = 'yes'", XMLtargets.item(y), XPathConstants.BOOLEAN); 
+						
+						//store the response in the hashmap
+						responses.put(name, isAuthorized);
 					}
 				}
 			}
 		}
 		//we had an error while trying to execute the request, assume not authorized
 		catch(Exception e) {
-			//output error encountered to the standard error stream before returning not authorized
+			//output error encountered to the standard error stream before generating response
 			e.printStackTrace(System.err);
-			return false;
 		}
-		// default to false
-		return false;
+
+		//generate response list
+		ArrayList<Boolean> response = new ArrayList<Boolean>(targets.size());
+		i = targets.iterator();
+		while(i.hasNext()) {
+			String target = i.next();
+			Boolean isAuthorized = responses.get(target.toLowerCase());
+			
+			//default to null if authorization response wasn't found
+			isAuthorized = (isAuthorized == null) ? Boolean.FALSE : isAuthorized;
+			
+			//add authorization response to response list
+			response.add(isAuthorized);
+		}
+		
+		//return the list of Boolean's
+		return response;
 	}
 
 	public boolean confluenceCheck(String entity, String spaceKey, String permissionType) {
@@ -135,6 +184,11 @@ public class PDP {
 	}
 
 	public void setGcxServerRoot(String gcxServerRoot) {
+		//trim trailing / if it exists
+		if(gcxServerRoot.endsWith("/")) {
+			gcxServerRoot = gcxServerRoot.substring(0, gcxServerRoot.length()-1);
+		}
+
 		this.gcxServerRoot = gcxServerRoot;
 	}
 }

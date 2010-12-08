542364540cfa460b7dcb63048c78085f39b7b440
Daniel Frett <daniel.frett@ccci.org>
2010-12-08 14:33:27 -0500

    updated the Check command object to utilize the new NullObject exceptions

--- src/main/java/org/ccci/gcx/authorization/command/Check.java
+++ src/main/java/org/ccci/gcx/authorization/command/Check.java
@@ -5,6 +5,9 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.ccci.gcx.authorization.Command;
+import org.ccci.gcx.authorization.exception.NullEntityException;
+import org.ccci.gcx.authorization.exception.NullObjectException;
+import org.ccci.gcx.authorization.exception.NullTargetException;
 import org.ccci.gcx.authorization.object.Entity;
 import org.ccci.gcx.authorization.object.Target;
 import org.w3c.dom.Document;
@@ -14,30 +17,32 @@ public final class Check extends Command {
     private final Entity entity;
     private final ArrayList<Target> targets;
 
-    public Check(final Entity entity, final List<Target> targets) {
+    public Check(final Entity entity, final List<Target> targets)
+	    throws NullObjectException {
 	this.entity = entity;
 	this.targets = new ArrayList<Target>(targets);
 
 	// throw an error if an invalid entity was specified
 	if (entity == null) {
-	    throw new IllegalArgumentException("null Entity was provided");
+	    throw new NullEntityException();
 	}
 	// throw an error if there are missing targets in the specified List
 	final Iterator<Target> t = this.targets.iterator();
 	while (t.hasNext()) {
 	    if (t.next() == null) {
-		throw new IllegalArgumentException("null Target in Target List");
+		throw new NullTargetException();
 	    }
 	}
     }
 
-    public Check(final Entity entity, final Target target) {
+    public Check(final Entity entity, final Target target)
+	    throws NullObjectException {
 	this(entity, new ArrayList<Target>());
 	this.targets.add(target);
 
 	// throw an error if there is a target isn't specified
 	if (target == null) {
-	    throw new IllegalArgumentException("null Target was provided");
+	    throw new NullTargetException();
 	}
     }
 

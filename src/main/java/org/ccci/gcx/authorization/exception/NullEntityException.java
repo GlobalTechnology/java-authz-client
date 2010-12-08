2823cbc6043378c85546976d868f9089d8a38a25
Daniel Frett <daniel.frett@ccci.org>
2010-12-08 14:32:00 -0500

    created a couple NullObject exception classes

---
+++ src/main/java/org/ccci/gcx/authorization/exception/NullEntityException.java
@@ -0,0 +1,5 @@
+package org.ccci.gcx.authorization.exception;
+
+public class NullEntityException extends NullObjectException {
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/org/ccci/gcx/authorization/exception/NullTargetException.java
@@ -0,0 +1,5 @@
+package org.ccci.gcx.authorization.exception;
+
+public class NullTargetException extends NullObjectException {
+    private static final long serialVersionUID = 1L;
+}

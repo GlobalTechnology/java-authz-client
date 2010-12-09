61db3ddc972db389a435f85ed8c924a75c912bf4
Daniel Frett <daniel.frett@ccci.org>
2010-12-09 16:32:26 -0500

    added support for id's on authorization commands

--- src/main/java/org/ccci/gcx/authorization/Command.java
+++ src/main/java/org/ccci/gcx/authorization/Command.java
@@ -4,7 +4,27 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 public abstract class Command {
+    private final String id;
+
+    public Command() {
+	this.id = Integer.toString(this.hashCode());
+    }
+
+    public Command(final String id) {
+	this.id = id;
+    }
+
+    /**
+     * @return the id for the current command object
+     */
+    public String getId() {
+	return this.id;
+    }
+
     public Element toXml(final Document doc) {
-	return doc.createElementNS(AuthzConstants.XMLNS, "command");
+	final Element cmd = doc
+		.createElementNS(AuthzConstants.XMLNS, "command");
+	cmd.setAttributeNS(null, "id", this.id);
+	return cmd;
     }
 }

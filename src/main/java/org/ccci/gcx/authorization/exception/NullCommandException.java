737eaa753abc6dc43751fa35e9e4ae992ad198b6
Daniel Frett <daniel.frett@ccci.org>
2010-12-09 20:18:12 -0500

    implemented a few command exceptions

---
+++ src/main/java/org/ccci/gcx/authorization/exception/NullCommandException.java
@@ -0,0 +1,5 @@
+package org.ccci.gcx.authorization.exception;
+
+public class NullCommandException extends Exception {
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/org/ccci/gcx/authorization/exception/NullCommandsException.java
@@ -0,0 +1,5 @@
+package org.ccci.gcx.authorization.exception;
+
+public class NullCommandsException extends Exception {
+    private static final long serialVersionUID = 1L;
+}

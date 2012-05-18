da0aba948a034e3d0b7bc54ee5d4f7005695900c
Daniel Frett <daniel.frett@ccci.org>
2012-05-18 17:10:27 -0400

    test to make sure namespaces are compared case-insensitively

--- src/test/java/org/ccci/gto/authorization/object/NamespaceTest.java
+++ src/test/java/org/ccci/gto/authorization/object/NamespaceTest.java
@@ -4,9 +4,9 @@ import junit.framework.TestCase;
 
 public class NamespaceTest extends TestCase {
     public void testEquals() {
-	final Namespace ns1 = new Namespace("test:namespace");
-	final Namespace ns2 = new Namespace("test:namespace");
-	assertEquals(ns1, ns2);
+        final Namespace ns1 = new Namespace("test:namespace");
+        final Namespace ns2 = new Namespace("TEST:NameSpace");
+        assertEquals(ns1, ns2);
     }
 
     public void testDescendant() {

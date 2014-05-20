bf6ff204d6a9245302c11869d56eedfb292ba6c9
Daniel Frett <daniel.frett@ccci.org>
2014-05-20 17:36:09 -0400

    use string switch instead of private enum

--- src/main/java/org/ccci/gto/authorization/response/AbstractListResponse.java
+++ src/main/java/org/ccci/gto/authorization/response/AbstractListResponse.java
@@ -1,17 +1,9 @@
 package org.ccci.gto.authorization.response;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-
 import org.ccci.gto.authorization.AuthzObject;
 import org.ccci.gto.authorization.Command;
 import org.ccci.gto.authorization.exception.InvalidXmlException;
+import org.ccci.gto.authorization.object.Attribute;
 import org.ccci.gto.authorization.object.Entity;
 import org.ccci.gto.authorization.object.Group;
 import org.ccci.gto.authorization.object.Key;
@@ -23,27 +15,15 @@ import org.ccci.gto.authorization.object.User;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public abstract class AbstractListResponse<T extends Command, O extends AuthzObject> extends AbstractResponse<T> {
     private final Class<O> objectClass;
     private final Collection<O> objects;
-
-    private enum ObjectType {
-        ENTITY, USER, GROUP, TARGET, RESOURCE, ROLE, NAMESPACE, KEY
-    }
-
-    private final static Map<String, ObjectType> OBJECTTYPEMAP;
-    static {
-        final Map<String, ObjectType> types = new HashMap<String, ObjectType>();
-        types.put("entity", ObjectType.ENTITY);
-        types.put("user", ObjectType.USER);
-        types.put("group", ObjectType.GROUP);
-        types.put("target", ObjectType.TARGET);
-        types.put("resource", ObjectType.RESOURCE);
-        types.put("role", ObjectType.ROLE);
-        types.put("namespace", ObjectType.NAMESPACE);
-        types.put("key", ObjectType.KEY);
-        OBJECTTYPEMAP = Collections.unmodifiableMap(types);
-    }
 
     public AbstractListResponse(final T command, final Integer code, final Class<O> objectClass,
             final Collection<O> objects) {
@@ -52,7 +32,7 @@ public abstract class AbstractListResponse<T extends Command, O extends AuthzObj
         if (objects == null || objects.size() == 0) {
             this.objects = Collections.emptyList();
         } else {
-            this.objects = Collections.unmodifiableCollection(new ArrayList<O>(objects));
+            this.objects = Collections.unmodifiableCollection(new ArrayList<>(objects));
         }
     }
 
@@ -78,33 +58,36 @@ public abstract class AbstractListResponse<T extends Command, O extends AuthzObj
                     final Element object = (Element) objectsNL.item(x);
 
                     // switch on node type
-                    switch (OBJECTTYPEMAP.get(object.getLocalName())) {
-                    case ENTITY:
-                        objects.add(objectClass.cast(new Entity(object)));
-                        break;
-                    case USER:
-                        objects.add(objectClass.cast(new User(object)));
-                        break;
-                    case GROUP:
-                        objects.add(objectClass.cast(new Group(object)));
-                        break;
-                    case TARGET:
-                        objects.add(objectClass.cast(new Target(object)));
-                        break;
-                    case RESOURCE:
-                        objects.add(objectClass.cast(new Resource(object)));
-                        break;
-                    case ROLE:
-                        objects.add(objectClass.cast(new Role(object)));
-                        break;
-                    case NAMESPACE:
-                        objects.add(objectClass.cast(new Namespace(object)));
-                        break;
-                    case KEY:
-                        objects.add(objectClass.cast(new Key(object)));
-                        break;
-                    default:
-                        throw new InvalidXmlException("unrecognized object");
+                    switch (object.getLocalName()) {
+                        case "entity":
+                            objects.add(objectClass.cast(new Entity(object)));
+                            break;
+                        case "user":
+                            objects.add(objectClass.cast(new User(object)));
+                            break;
+                        case "group":
+                            objects.add(objectClass.cast(new Group(object)));
+                            break;
+                        case "target":
+                            objects.add(objectClass.cast(new Target(object)));
+                            break;
+                        case "resource":
+                            objects.add(objectClass.cast(new Resource(object)));
+                            break;
+                        case "role":
+                            objects.add(objectClass.cast(new Role(object)));
+                            break;
+                        case "namespace":
+                            objects.add(objectClass.cast(new Namespace(object)));
+                            break;
+                        case "key":
+                            objects.add(objectClass.cast(new Key(object)));
+                            break;
+                        case "attribute":
+                            objects.add(objectClass.cast(new Attribute(object)));
+                            break;
+                        default:
+                            throw new InvalidXmlException("unrecognized object");
                     }
                 }
             } catch (final ClassCastException e) {
